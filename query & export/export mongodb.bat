6fecba3a8f318b0d6f98576edc3cd1add5e78f16
Do <cdo7@csc.com>
2017-12-06 16:59:47 +0700

    database after add reservation feature

--- query & export/export mongodb.bat
+++ query & export/export mongodb.bat
@@ -1,9 +1,12 @@
-cd C:\Program Files\MongoDB\Server\3.4\bin\
+C:
 
-mongoexport -d HotelBookingReservationsSystem -c rooms -o D:/rooms.json
-mongoexport -d HotelBookingReservationsSystem -c customers -o D:/customers.json
-mongoexport -d HotelBookingReservationsSystem -c activity -o D:/activity.json
-mongoexport -d HotelBookingReservationsSystem -c admin -o D:/admin.json
-mongoexport -d HotelBookingReservationsSystem -c follow-users -o D:/follow-users.json
-mongoexport -d HotelBookingReservationsSystem -c hotel-services -o D:/hotel-services.json
-mongoexport -d HotelBookingReservationsSystem -c ip-suggest -o D:/ip-suggest.json
+cd C:\Program Files\MongoDB\Server\3.4\bin
+
+mongoexport -d HotelBookingReservationsSystem -c rooms -o D:\rooms.json
+mongoexport -d HotelBookingReservationsSystem -c customers -o D:\customers.json
+mongoexport -d HotelBookingReservationsSystem -c activity -o D:\activity.json
+mongoexport -d HotelBookingReservationsSystem -c admin -o D:\admin.json
+mongoexport -d HotelBookingReservationsSystem -c follow-users -o D:\follow-users.json
+mongoexport -d HotelBookingReservationsSystem -c restaurant -o D:\restaurant.json
+mongoexport -d HotelBookingReservationsSystem -c ip-suggest -o D:\ip-suggest.json
+mongoexport -d HotelBookingReservationsSystem -c test -o D:\test.json
